9e51d8a2b891da84900365ab36bc452d4a3caff5
Pritam <pri10145@gmail.com>
2023-05-19 19:52:11 +0530

    Add Link in buttons

--- client/src/container/Hero.jsx
+++ client/src/container/Hero.jsx
@@ -1,4 +1,5 @@
 import { Banner } from "../assets";
+import {Link} from 'react-router-dom'
 
 const Hero = () => {
   return (
@@ -9,9 +10,9 @@ const Hero = () => {
             <h1 className="w-full sm:max-w-lg font-clash font-normal text-3xl sm:text-4xl text-white">
               The furniture brand for the future, with timeless designs
             </h1>
-            <button className="hidden md:block bg-white bg-opacity-20 font-satoshi font-normal text-light-gray px-5 py-2 hover:bg-opacity-25 transition-all duration-200 focus:bg-opacity-25 outline-none">
+            <Link to='/products' className="hidden md:block bg-white bg-opacity-20 font-satoshi font-normal text-light-gray px-5 py-2 hover:bg-opacity-25 transition-all duration-200 focus:bg-opacity-25 outline-none cursor-pointer">
               View Collection
-            </button>
+            </Link>
           </div>
           <div className="w-full sm:max-w-xl flex flex-col items-start justify-start gap-10">
             <p className="font-satoshi text-sm sm:text-base text-white">
@@ -19,9 +20,9 @@ const Hero = () => {
               retail brand with nice fonts, tasteful colors and a beautiful way
               to display things digitally using modern web technologies.
             </p>
-            <button className="block md:hidden w-full bg-white bg-opacity-20 font-satoshi font-normal text-light-gray px-5 py-2 hover:bg-opacity-25 transition-all duration-200 focus:bg-opacity-25 outline-none ">
+            <Link to='/products' className="block md:hidden w-full bg-white bg-opacity-20 font-satoshi font-normal text-light-gray px-5 py-2 hover:bg-opacity-25 transition-all duration-200 focus:bg-opacity-25 outline-none cursor-pointer">
               View Collection
-            </button>
+            </Link>
           </div>
         </section>
         <section className="w-[620px] h-[440px] hidden md:block">
