9006c66d85ac7eab16c35a8e283b017162571452
Pritam <pri10145@gmail.com>
2023-05-19 20:32:43 +0530

    div replaced by section

--- client/src/container/Features.jsx
+++ client/src/container/Features.jsx
@@ -7,9 +7,9 @@ import { FeaturesCard } from "../components";
 const Features = () => {
   return (
     <>
-      <div className="w-full h-full flex flex-col items-center justify-center py-10">
+      <section className="w-full h-full flex flex-col items-center justify-center py-10">
         <h1 className="font-clash font-normal text-2xl text-Primary">What makes our brand different</h1>
-      </div>
+      </section>
       <section className="w-full xl:w-[1260px] h-full mx-auto flex flex-wrap items-center justify-center lg:justify-between gap-16 lg:gap-10 px-7 xl:px-0 p-5">
         <FeaturesCard
           icon={TbTruckDelivery}
