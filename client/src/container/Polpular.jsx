b164149e3a2aafcaf91a894a695bb686e65f6f51
Pritam <pri10145@gmail.com>
2023-05-20 07:42:15 +0530

    add button on Polpular products section

--- client/src/container/Polpular.jsx
+++ client/src/container/Polpular.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import { ProductCard } from "../components"
 import { popular } from "../constants"
 
@@ -14,6 +15,14 @@ const Polpular = () => {
             <ProductCard key={ceramic.id} ceramic={ceramic} />
           ))}
         </section>
+        <Link
+          to="/products"
+          className="w-full flex items-center justify-center"
+        >
+          <button className="bg-light-gray text-Primary font-satoshi font-normal text-base px-6 py-3 outline-none border-none ">
+            View collection
+          </button>
+        </Link>
     </div>
     </>
   )
