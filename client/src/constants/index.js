645b7edeb214ac253d36337ba8b09faa83c3931f
Pritam <pri10145@gmail.com>
2023-05-20 15:30:49 +0530

    AllProducts adds

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -48,3 +48,49 @@ export const popular = [
     },
 
 ] 
+
+export const AllProducts = [
+    {
+        id: 1,
+        icon: chair,
+        name: 'The Dandy chair',
+        price: '4000'
+        tag: 'ch'
+    },
+    {
+        id: 2,
+        icon: vase,
+        name: 'Rustic Vase Set',
+        price: '2200'
+    },
+    {
+        id: 3,
+        icon: silky,
+        name: 'The Silky Vase',
+        price: '2500'
+    },
+    {
+        id: 4,
+        icon: lamp,
+        name: 'The Lucy Lamp',
+        price: '3400'
+    },
+    {
+        id: 5,
+        icon1: sofa,
+        name: 'The Lucy Lamp',
+        price: '5400'
+    },
+    {
+        id: 6,
+        icon: chair2,
+        name: 'The Lucy Lamp',
+        price: '5400'
+    },
+    {
+        id: 7,
+        icon: lamp1,
+        name: 'The Lucy Lamp',
+        price: '5400'
+    },
+]
