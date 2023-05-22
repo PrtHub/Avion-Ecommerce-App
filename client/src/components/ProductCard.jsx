9151cdf571f87ad5236f6d134d8d7812fa9fb904
Pritam <pri10145@gmail.com>
2023-05-22 15:26:11 +0530

    add functionality to img

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,8 +1,14 @@
 /* eslint-disable react/prop-types */
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import {motion} from 'framer-motion'
 
 const ProductCard = ({ ceramic }) => {
+  const navigate = useNavigate();
+
+  const handleNextPage = () => {
+    window.scrollTo(0, 0);
+    navigate(`/product/${ceramic.id}`)
+  }
   return (
     <>
       <motion.div
@@ -10,21 +16,21 @@ const ProductCard = ({ ceramic }) => {
         scale: 1.05,
       }}
       className={`${ceramic.icon1 ? 'w-[603px]': ' w-[300px]'} h-[460px] bg-white flex flex-col items-start justify-start gap-2`}>
-        <Link to={`/product/${ceramic.id}`}>
+        <div onClick={handleNextPage}>
           {ceramic.icon1 ? (
             <img
               src={ceramic.icon1}
               alt="ceramic image"
-              className="w-full h-[375px] object-cover"
+              className="w-full h-[375px] object-cover cursor-pointer"
             />
           ) : (
             <img
               src={ceramic.icon}
               alt="ceramic image"
-              className="w-full h-[375px] object-cover"
+              className="w-full h-[375px] object-cover cursor-pointer"
             />
           )}
-        </Link>
+        </div>
         <h3 className="font-clash font-normal text-xl text-Primary">
           {ceramic.name}
         </h3>
