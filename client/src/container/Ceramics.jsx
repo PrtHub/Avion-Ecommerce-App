c2795380ed30ec3f11f519c256775134f8ca6a95
Pritam <pri10145@gmail.com>
2023-05-22 15:29:05 +0530

    add ScrollTop

--- client/src/container/Ceramics.jsx
+++ client/src/container/Ceramics.jsx
@@ -1,8 +1,14 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { ProductCard } from "../components";
 import { ceremics } from "../constants";
 
 const Ceramics = () => {
+  const navigate = useNavigate();
+
+  const handleScroll = () => {
+    window.scrollTo(0, 0);
+    navigate('/products')
+  }
   return (
     <>
       <div className="content-wrapper flex flex-col items-start justify-start">
@@ -17,14 +23,14 @@ const Ceramics = () => {
             <ProductCard key={ceramic.id} ceramic={ceramic} />
           ))}
         </section>
-        <Link
-          to="/products"
+        <div
+          onClick={handleScroll}
           className="w-full flex items-center justify-center"
         >
           <button className="bg-light-gray text-Primary font-satoshi font-normal text-base px-6 py-3 outline-none border-none ">
             View collection
           </button>
-        </Link>
+        </div>
       </div>
     </>
   );
