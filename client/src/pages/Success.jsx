1e2d150e1a185a4e747883311b8ce3054c3a796a
Pritam <pri10145@gmail.com>
2023-05-23 10:13:18 +0530

    Connect canvas-confetti to Success page

--- client/src/pages/Success.jsx
+++ client/src/pages/Success.jsx
@@ -1,5 +1,6 @@
 import { BsBagCheckFill } from 'react-icons/bs'
 import {useNavigate} from 'react-router-dom'
+import { Canvas } from '../utils'
 
 const Success = () => {
 const navigate = useNavigate()
@@ -11,6 +12,7 @@ const navigate = useNavigate()
     <>
     <div className="content-wrapper flex items-center justify-center py-20">
        <section className="w-auto h-60 bg-light-gray flex flex-col items-center justify-center gap-4" >
+        <Canvas/>
           <BsBagCheckFill className='text-Primary w-10 h-10'/> 
           <h1 className='text-Primary text-4xl font-clash font-medium'>Thank You for Purchasing </h1>
           <p className='max-w-sm text-center text-light-gray-100 font-normal font-satoshi'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut minima nostrum porro dolore deserunt similique.</p>
