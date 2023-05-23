06e0b7c5b7c06db6feab724ba7d886629c3996d0
Pritam <pri10145@gmail.com>
2023-05-23 10:15:06 +0530

    text-center

--- client/src/pages/Success.jsx
+++ client/src/pages/Success.jsx
@@ -11,10 +11,10 @@ const navigate = useNavigate()
   return (
     <>
     <div className="content-wrapper flex items-center justify-center py-20">
-       <section className="w-auto h-60 bg-light-gray flex flex-col items-center justify-center gap-4" >
+       <section className="w-auto h-auto bg-light-gray flex flex-col items-center justify-center gap-4" >
         <Canvas/>
           <BsBagCheckFill className='text-Primary w-10 h-10'/> 
-          <h1 className='text-Primary text-4xl font-clash font-medium'>Thank You for Purchasing </h1>
+          <h1 className='text-Primary text-4xl font-clash font-medium text-center'>Thank You for Purchasing </h1>
           <p className='max-w-sm text-center text-light-gray-100 font-normal font-satoshi'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut minima nostrum porro dolore deserunt similique.</p>
           <button className='bg-Primary text-white font-satoshi font-normal px-6 py-3' onClick={goToProducts}>Continue Shopping</button>
        </section>
