ace2da00df66ad26ea2708b33da0cc003fe19e90
Pritam <pri10145@gmail.com>
2023-05-23 10:05:50 +0530

    navigate to products page

--- client/src/pages/Success.jsx
+++ client/src/pages/Success.jsx
@@ -1,6 +1,12 @@
 import { BsBagCheckFill } from 'react-icons/bs'
+import {useNavigate} from 'react-router-dom'
 
 const Success = () => {
+const navigate = useNavigate()
+    const goToProducts = () => {
+      window.scrollTo(0, 0);
+      navigate('/products')
+    }
   return (
     <>
     <div className="content-wrapper flex items-center justify-center py-20">
@@ -8,7 +14,7 @@ const Success = () => {
           <BsBagCheckFill className='text-Primary w-10 h-10'/> 
           <h1 className='text-Primary text-4xl font-clash font-medium'>Thank You for Purchasing </h1>
           <p className='max-w-sm text-center text-light-gray-100 font-normal font-satoshi'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut minima nostrum porro dolore deserunt similique.</p>
-          <button className='bg-Primary text-white font-satoshi font-normal px-6 py-3'>Continue Shopping</button>
+          <button className='bg-Primary text-white font-satoshi font-normal px-6 py-3' onClick={goToProducts}>Continue Shopping</button>
        </section>
     </div>
     </>
